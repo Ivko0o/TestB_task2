433e5b5969a0bdac3202bf07576b049dcc676f08
Ivko0o <IvelinVasilev91@gmail.com>
2024-11-28 12:19:46 +0200

    Improved logic in main function

--- TestB_task2.cpp
+++ TestB_task2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,14 +7,49 @@ bool isPrime(int a);
 
 int main()
 {
-    int firstPrime = 0;
-    int secondPrime = 0;
-    int n = 0;
+    int n;
+    cout << "Enter a number between 1 and 100,000,000: ";
+    cin >> n;
 
-    cout << isPrime(9);
+    //Makes sure that the input data is in correct bracket
+    while (n < 1 || n > 100000000) {
+        cout << "Invalid input! Try again!" << endl;
+        cin >> n;
+    }
+
+    //This loop checks all numbers if they are doubleTwinned
+    for (int x = 4; x < n; ++x) {
+
+        //Checks if the number is exact square number
+        int root = sqrt(x);
+        if (root * root != x) {
+            continue;
+        }
+
+        //Checks if the number is Twinned (the sum of two Prime numbers)
+        bool doubleTwinned = false;
+        for (int p1 = 2; p1 < x; ++p1) {
+            if (isPrime(p1)) {
+                int p2 = x - p1;
+                //Checks if the number is DoubleTwinned
+                if (p2 > p1 && isPrime(p2)) {
+                    doubleTwinned = true;
+                    break;
+                }
+            }
+        }
+
+        //if the number is DoubleTwinned gets printed
+        if (doubleTwinned) {
+            cout << x << endl;
+        }
+    }
+
+    return 0;
 
 }
 
+//This function will check if a number is prime
 bool isPrime(int a) {
 
     int count = 0;
